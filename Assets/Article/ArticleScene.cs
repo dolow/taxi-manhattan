9d8c76b70304813fd84222f1c2cd3cbcfa7ebd68
dolow <velvet.nanashi@gmail.com>
2021-03-05 21:24:13 +0900

    added sample scene for article

---
+++ Assets/Article/ArticleScene.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Article
+{
+    [Serializable]
+    public struct Map
+    {
+        public int width;
+        public int height;
+        public int[] fields;
+    }
+
+    public class ArticleScene : MonoBehaviour
+    {
+        [SerializeField]
+        private Tile tilePool;
+
+        private Map map;
+
+        private void Start()
+        {
+            TextAsset mapAsset = Resources.Load<TextAsset>("Article/ArticleField");
+            this.map = JsonUtility.FromJson<Map>(mapAsset.text);
+
+            for (int i = 0; i < this.map.fields.Length; i++)
+            {
+                int tileId = map.fields[i];
+                Vector3 tilePos = new Vector3(i % map.width, 0.0f, -Mathf.Floor(i / map.width));
+                Tile tile = GameObject.Instantiate<Tile>(this.tilePool);
+                tile.SetTilePos(tileId);
+                // tile.UpdateUV();
+                tile.transform.position = tilePos;
+                tile.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Article/Tile.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Article
+{
+    public class Tile : MonoBehaviour
+    {
+        [SerializeField]
+        private Vector2 tileCount = Vector2.zero;
+        [SerializeField]
+        private Vector2 tilePos = Vector2.zero;
+
+        private void Start()
+        {
+            this.UpdateUV();
+        }
+
+        public void SetTilePos(int index)
+        {
+            Vector2 pos = new Vector2(index % this.tileCount.x, Mathf.Floor(index / this.tileCount.x));
+            this.tilePos = pos;
+        }
+
+        public void UpdateUV()
+        {
+            Renderer renderer = this.GetComponent<Renderer>();
+
+            Vector2 tileScale = new Vector2(1.0f / this.tileCount.x, 1.0f / this.tileCount.y);
+            Vector2 offset = new Vector2(this.tilePos.x * tileScale.x, 1.0f - tileScale.y - this.tilePos.y * tileScale.y);
+            renderer.material.SetTextureScale("_MainTex", tileScale);
+            renderer.material.SetTextureOffset("_MainTex", offset);
+        }
+    }
+}
